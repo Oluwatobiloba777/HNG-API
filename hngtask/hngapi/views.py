7e1ad09c02f9c09231cc39189ec08163068bfbc9
Oluwatobiloba <iamtobihunkuten@gmail.com>
2023-09-07 14:04:45 +0100

    initial commit

---
+++ hngtask/hngapi/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import JsonResponse
+from datetime import datetime
+import calendar
+
+
+def task(request):
+    name = request.GET.get('slack_name', 'Oluwatobiloba777')
+    track = request.GET.get('track', 'backend')
+    utc_time = datetime.now()
+    current_day = calendar.day_name[utc_time.weekday()]
+
+    dicct = {
+    "slack_name": name,
+    "current_day": current_day,
+    "utc_time": utc_time,
+    "track": track,
+    "github_file_url": "git file link",
+    "github_repo_url": "https://github.com/Oluwatobiloba777/HNG-API",
+}
+    return JsonResponse(dicct, safe=False)
